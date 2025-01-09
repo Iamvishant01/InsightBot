ac2ad6402a8aa13172dc4807754b58cec597402a
Vishant <iamvishant01@gmail.com>
2025-01-09 20:55:50 +0530

    Api changes

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 // Gemini
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const genAI = new GoogleGenerativeAI(process.env.API_Key);
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
